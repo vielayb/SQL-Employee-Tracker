9718f56894c2ed1c2789346c554e8637610897b2
Vielay Bangphaxay <pounds@Vielays-MacBook-Pro.local>
2021-10-29 11:54:58 -0400

    foreign key

--- server.js
+++ server.js
@@ -116,6 +116,88 @@ app.post('/api/employee', ({ body }, res) => {
   });
 });
 
+// Update a candidate's party
+app.put('/api/employee/:id', (req, res) => {
+  const errors = inputCheck(req.body, 'role_id');
+
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  const sql = `UPDATE employees SET role_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.role_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Role not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
+
+// Get all roles
+app.get('/api/roles', (req, res) => {
+  const sql = `SELECT * FROM roles`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+// Get a single role
+app.get('/api/role/:id', (req, res) => {
+  const sql = `SELECT * FROM roles WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+app.delete('/api/role/:id', (req, res) => {
+  const sql = `DELETE FROM roles WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Role not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
+
 // Create a employee
 // const sql = `INSERT INTO employees (id, first_name, last_name, industry_connected) 
 //               VALUES (?,?,?,?)`;
